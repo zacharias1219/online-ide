9b9a3c70b5b005a33b10d4720a97527d2882008b
Richard Abishai <rickoshade1891@gmail.com>
2025-01-04 23:27:44 +0530

    Implement Header and loading skeleton components for editor and output panels

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,16 +1,16 @@
-// import EditorPanel from "./_components/EditorPanel";
-// import Header from "./_components/Header";
-// import OutputPanel from "./_components/OutputPanel";
+import EditorPanel from "./_components/EditorPanel";
+import Header from "./_components/Header";
+import OutputPanel from "./_components/OutputPanel";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <div className="max-w-[1800px] mx-auto p-4">
-        {/* <Header /> */}
+        <Header />
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-          {/* <EditorPanel />
-          <OutputPanel /> */}
+          <EditorPanel />
+          <OutputPanel />
         </div>
       </div>
     </div>
